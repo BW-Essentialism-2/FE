b9137dc72bc1838ae8089ad1650a5243ebad2f9b
DustinG98 <thedustingraham@gmail.com>
2020-03-01 20:14:49 -0700

    Revert "Merge pull request #4 from BW-Essentialism-2/Javier-Gutierrez"
    This reverts commit 08fc616ae6d8781c351452534e487cd3a410cf70, reversing
    changes made to 9ac794170b9f75d590d7d13a8459faf91ee0ed0a.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Logo from './Img/logo.png'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Coming Soon! </h1>
- <img  src={ require('./Img/logo.png') } />
+
     </div>
   );
 }
