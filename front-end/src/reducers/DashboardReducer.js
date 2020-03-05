dcfab95b591e81935f90747c9f1531afe87fb523
hsisco <hysensisco@gmail.com>
2020-03-04 18:53:17 -0800

    Clarify reducer actions

--- front-end/src/reducers/DashboardReducer.js
+++ front-end/src/reducers/DashboardReducer.js
@@ -1,27 +1,54 @@
-import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from '../actions/Dashboard';
+import { FETCH_DATA, 
+        DATA_SUCCESS, 
+        DATA_FAILURE, 
+        FETCH_PROJECTS, 
+        PROJECTS_SUCCESS, 
+        PROJECTS_FAILURE } from '../actions/Dashboard';
 
 const initialState = {
-  userData: [],
+  allValues: [],
+  importantValues: [],
+  top3: [],
   isFetching: false,
   error: ''
 };
 
 const DashboardReducer = (state = initialState, action) => {
   switch (action.type) {
-    case START_FETCHING:
+    case FETCH_DATA:
       return {
         ...state,
         isFetching: true,
         error: ''
       };
-    case FETCH_SUCCESS:
+    case DATA_SUCCESS:
       return {
         ...state,
         isFetching: false,
         error: '',
         smurfData: action.payload
       };
-    case FETCH_FAILURE:
+    case DATA_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false
+      };
+      
+    case FETCH_PROJECTS:
+      return {
+        ...state,
+        isFetching: true,
+        error: ''
+      };
+    case PROJECTS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        error: '',
+        smurfData: action.payload
+      };
+    case PROJECTS_FAILURE:
       return {
         ...state,
         error: action.payload,
