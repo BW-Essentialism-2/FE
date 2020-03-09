c1610dfd05e895e2d3df3e20a02984e147233c81
hsisco <hysensisco@gmail.com>
2020-03-08 23:08:39 -0700

    Project form

--- front-end/src/components/dashboard/DashboardEdit.js
+++ front-end/src/components/dashboard/DashboardEdit.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { 
   FETCH_USER_VALUES_START, 
   UPDATE_USER_VALUES_START, 
   FETCH_PROJECTS_START, 
   ADD_PROJECT_START, 
-  UPDATE_PROJECTS_START, 
+  // UPDATE_PROJECTS_START, 
   DELETE_PROJECT_START
 } from '../../actions/Dashboard';
 
@@ -24,8 +24,11 @@ const DashboardEdit = () => {
   // TOP 3
   const importantValues = state.allValues.filter(value => value.important === true);
   const [ top3, setTop3 ] = useState({
-    value: {id: `${state.value_id}`, comment: `${state.comment}`}
+    value: {value_id: `${state.value_id}`, comment: `${state.comment}`}
   });
+
+  const handleChangeValue = e => setTop3({...top3, [e.target.name]: {...top3[e.target.name], id: e.target.value}});
+  const handleChangeComment = e => setTop3({...top3, [e.target.name]: {...top3[e.target.name], comment: e.target.value}});
 
   const handleSubmitTop3 = () => {
     let v1 = state.allValues.filter(value => value.value_id === Number(top3.v1.id))[0];
@@ -44,8 +47,6 @@ const DashboardEdit = () => {
     dispatch(UPDATE_USER_VALUES_START(v3));
     // console.log(state);
   };
-  const handleChangeValue = e => setTop3({...top3, [e.target.name]: {...top3[e.target.name], id: e.target.value}});
-  const handleChangeComment = e => setTop3({...top3, [e.target.name]: {...top3[e.target.name], comment: e.target.value}});
 
   // PROJECTS
   const topValues = state.userValues.filter(value => value.top3 === true);
@@ -63,22 +64,22 @@ const DashboardEdit = () => {
   const handleChangeProject = e => setProject({...project, name: e.target.value});
   const handleChangeProjectValue = e => setProject({...project, value_id: e.target.value});
 
-  // ALL VALUES
-  // const [ value, setValue ] = useState();
-
-  const handleSubmitProject = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     history.push('/protected');
   }
   
+  // ALL VALUES
+  // const [ value, setValue ] = useState();
+
   return (
     <div className="dashboard-edit">
       <div className="top3-edit">
         <h2>Update Your Top Priorities</h2>
         <form onSubmit={handleSubmitTop3}>
           <select onChange={handleChangeValue}>
-            <option selected value>Choose</option>
-            {importantValues.map(value => {
+            <option selected value>{top3.value_id}</option>
+            {topValues.map(value => {
               return(
               <option key={value.value_id} {...value} >
                 {value.value}
@@ -89,11 +90,45 @@ const DashboardEdit = () => {
           <textarea
             name="topValue"
             value={top3.topValue.comment}
-            onChange={handleChangeComment}/>
+            onChange={handleChangeProject}/>
         </form>
-        <button type="submit" onClick={handleSubmitTop3}>Save</button>
+        <button type="submit" onClick={handleSubmitTop3}>Save Priorities</button>
+      </div>
+      <div>
+        <h2>Update Your Projects</h2>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            placeholder="New project"
+            value={project.project}
+            onChange={handleChangeProject}
+          />
+          <select 
+            name="projectValue"
+            value={project.value_id}
+            onChange={handleChangeProjectValue}>
+            <option selected value>Choose Value</option>
+              {importantValues.map(value => {
+                return(
+                <option key={value.value_id} {...value} >
+                  {value.value}
+                </option>
+                )
+              })}
+          </select>
+          <button onClick={addProject}>Save Project</button>
+        </form>
+        {state.projects.map(project => {
+                return(
+                  <div key={project.project_id} {...project} >
+                    <h3>{project.project}</h3>
+                    <p onClick={deleteProject}>x</p>
+                  </div>
+                )
+              })}
 
       </div>
+      <button type="submit" onClick={handleSubmit}>Save</button>
     </div>
   )
 };
